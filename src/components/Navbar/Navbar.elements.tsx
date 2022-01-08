8dc9d1f8c934d987e33da8aa232c4a361a71b5b4
PrinceNarteh <prinarteh@gmail.com>
2022-01-08 06:16:07 +0000

    add styled components

---
+++ src/components/Navbar/Navbar.elements.tsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const Nav = styled.nav`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 80px;
+  background: #101522;
+  font-size: 1.2rem;
+  position: sticky;
+  top: 0;
+  z-index: 999;
+`;
---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,11 @@
+import { Nav } from "./Navbar.elements";
+
+const Navbar = () => {
+  return (
+    <Nav>
+      <h1>Navbar</h1>
+    </Nav>
+  );
+};
+
+export default Navbar;
